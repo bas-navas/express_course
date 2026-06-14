5bb0e87e984a2e5885815200def7a91e7e91964b
banana-bas <thebashp4@gmail.com>
2026-06-14 18:43:20 +0700

    refactor: simplify

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -2,14 +2,21 @@ import { BrowserRouter, Routes, Route } from 'react-router-dom'
 import Login from './pages/Login'
 import Users from './pages/Users'
 import Register from './pages/Register'
+import ProtectedRoute from './components/ProtectedRoute'
 
 function App() {
   return (
     <BrowserRouter>
       <Routes>
         <Route path='/' element={<Login />} />
-        <Route path='/register' element={<Register/>}/>
-        <Route path='/users' element={<Users/>}/>
+        <Route path='/register' element={<Register />} />
+
+        <Route path='/users' element={
+          <ProtectedRoute>
+            <Users />
+          </ProtectedRoute>
+        } />
+
       </Routes>
     </BrowserRouter>
   )
